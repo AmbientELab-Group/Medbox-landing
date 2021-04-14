d11dbbca000b9e2cc25f4f5f8500f81e1ba52e4c
BalboBigguns <micholyea@gmail.com>
2021-04-14 18:08:38 +0200

    Added About us section.

---
+++ src/components/slider.js
@@ -0,0 +1,165 @@
+import React, { useState, useRef } from "react";
+import styled from "styled-components";
+import RightArrowIcon from "../assets/right-arrow.svg";
+import LeftArrowIcon from "../assets/left-arrow.svg";
+import { motion } from "framer-motion";
+
+const SliderContainer = styled.div`
+  height: 100%;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  overflow: auto;
+`;
+
+const SlideWrapper = styled.div`
+  height: 100%;
+  flex: 0.8;
+  overflow: hidden;
+  position: relative;
+`;
+
+const ChildrenWrapper = styled(motion.div)`
+  height: 100%;
+  width: 100%;
+  position: absolute;
+`;
+
+const SliderSide = styled.div`
+  height: 100%;
+  flex: 0.1;
+  min-width: 40px;
+  display: flex;
+  align-items: center;
+
+  @media (min-width: ${({ theme }) => theme.breakpoints.lg}) {
+    max-width: 80px;
+  }
+`;
+
+const ArrowWrapper = styled.div`
+  height: 100px;
+  width: 100px;
+  display: flex;
+  cursor: pointer;
+`;
+
+const ChildrenVariants = {
+  prev: {
+    x: "-100%",
+    transition: {
+      type: "tween",
+    },
+  },
+  visible: {
+    x: 0,
+    transition: {
+      type: "tween",
+    },
+  },
+  next: {
+    x: "100%",
+    transition: {
+      type: "tween",
+    },
+  },
+};
+
+const getChildrenRight = (children, visibleIdx) => (
+  <>
+    <ChildrenWrapper
+      initial="visible"
+      animate="prev"
+      variants={ChildrenVariants}
+      key={visibleIdx - 1}
+    >
+      {children[visibleIdx - 1]}
+    </ChildrenWrapper>
+    <ChildrenWrapper
+      initial="next"
+      animate="visible"
+      variants={ChildrenVariants}
+      key={visibleIdx}
+    >
+      {children[visibleIdx]}
+    </ChildrenWrapper>
+  </>
+);
+
+const getChildrenLeft = (children, visibleIdx) => (
+  <>
+    <ChildrenWrapper
+      initial="prev"
+      animate="visible"
+      variants={ChildrenVariants}
+      key={visibleIdx}
+    >
+      {children[visibleIdx]}
+    </ChildrenWrapper>
+    <ChildrenWrapper
+      initial="visible"
+      animate="next"
+      variants={ChildrenVariants}
+      key={visibleIdx + 1}
+    >
+      {children[visibleIdx + 1]}
+    </ChildrenWrapper>
+  </>
+);
+
+const getChildrenCount = (children) => {
+  if (!children) {
+    throw Error("Slider requires at least one child element.");
+  }
+
+  let itemCount = 1;
+  if (Array.isArray(children)) {
+    itemCount = children.length;
+  }
+  return itemCount;
+};
+
+const Slider = ({ children, className }) => {
+  const [visibleIdx, setVisibleIdx] = useState(0);
+  const itemCount = useRef(getChildrenCount(children));
+  const [activeChildren, setActiveChildren] = useState(() => (
+    <ChildrenWrapper>
+      {itemCount.current === 1 ? children : children[0]}
+    </ChildrenWrapper>
+  ));
+
+  const goNext = () => {
+    const newIdx =
+      visibleIdx < itemCount.current - 1 ? visibleIdx + 1 : visibleIdx;
+    setVisibleIdx(newIdx);
+    setActiveChildren(getChildrenRight(children, newIdx));
+  };
+  const goBack = () => {
+    const newIdx = visibleIdx > 0 ? visibleIdx - 1 : visibleIdx;
+    setVisibleIdx(newIdx);
+    setActiveChildren(getChildrenLeft(children, newIdx));
+  };
+
+  return (
+    <SliderContainer className={className}>
+      <SliderSide>
+        {visibleIdx !== 0 && (
+          <ArrowWrapper>
+            <LeftArrowIcon onClick={goBack} />
+          </ArrowWrapper>
+        )}
+      </SliderSide>
+      <SlideWrapper>{activeChildren}</SlideWrapper>
+      <SliderSide>
+        {visibleIdx !== itemCount.current - 1 && (
+          <ArrowWrapper>
+            <RightArrowIcon onClick={goNext} />
+          </ArrowWrapper>
+        )}
+      </SliderSide>
+    </SliderContainer>
+  );
+};
+
+export default Slider;
--- src/sections/about.js
+++ src/sections/about.js
@@ -1,8 +1,79 @@
 import React from "react";
+import styled from "styled-components";
 import Section from "../components/section";
+import { useTranslation } from "react-i18next";
+import Title from "../components/typography/title";
+import TeamMember from "../components/teamMember";
+import { useStaticQuery, graphql } from "gatsby";
+import Slider from "../components/slider";
+
+const SectionLayout = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 1rem;
+
+  @media (min-width: ${({ theme }) => theme.breakpoints.lg}) {
+    padding: 4rem;
+  }
+`;
+
+const MembersWrapper = styled.div`
+  display: flex;
+  height: 100%;
+  width: 100%;
+  align-content: center;
+  justify-content: center;
+  flex-wrap: wrap;
+`;
 
 const AboutSection = ({ id }) => {
-  return <Section id={id}></Section>;
+  const { t } = useTranslation();
+  const { allMembersJson } = useStaticQuery(graphql`
+    query AboutSectionQuery {
+      allMembersJson {
+        edges {
+          node {
+            members {
+              name
+              role
+              photo {
+                childImageSharp {
+                  gatsbyImageData(placeholder: BLURRED)
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  `);
+  const membersPages = allMembersJson.edges.map((edge) => edge.node.members);
+  console.log(membersPages);
+
+  return (
+    <Section id={id}>
+      <SectionLayout>
+        <Title>{t("sections.about.title")}</Title>
+        <Slider>
+          {membersPages.map((page) => (
+            <MembersWrapper>
+              {page.map((member) => (
+                <TeamMember
+                  key={member.name}
+                  name={member.name}
+                  role={t(member.role)}
+                  photo={member.photo}
+                  photoAlt={t("Image of ") + member.name}
+                />
+              ))}
+            </MembersWrapper>
+          ))}
+        </Slider>
+      </SectionLayout>
+    </Section>
+  );
 };
 
 export default AboutSection;
