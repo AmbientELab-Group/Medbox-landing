2ef00f38e9c58787c3017876669c8306ba032c52
BalboBigguns <micholyea@gmail.com>
2021-06-10 22:34:27 +0200

    Added about us page.

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,6 +4,8 @@ import styled from "styled-components";
 import Layout from "../components/layout";
 import { useTranslation } from "gatsby-plugin-react-i18next";
 import SEO from "../components/SEO";
+import Title from "../components/typography/title";
+import Introduction from "../components/introduction";
 
 const PageContainer = styled.div`
   display: flex;
@@ -14,17 +16,24 @@ const PageContainer = styled.div`
 
   @media (min-width: ${({ theme }) => theme.breakpoints.lg}) {
     padding: 4rem 4rem;
-    flex-direction: row;
   }
 `;
 
-const AboutPage = () => {
+const AboutPage = ({ data }) => {
   const { t } = useTranslation("about");
+  const introductions = data.introductions.edges.map((edge) => ({
+    ...edge.node,
+  }));
 
   return (
     <Layout>
       <SEO />
-      <PageContainer></PageContainer>
+      <PageContainer>
+        <Title>{t("title")}</Title>
+        {introductions.map((intro) => (
+          <Introduction content={intro} key={intro.title} />
+        ))}
+      </PageContainer>
     </Layout>
   );
 };
@@ -41,6 +50,19 @@ export const query = graphql`
           ns
           data
           language
+        }
+      }
+    }
+    introductions: allIntroductionsJson {
+      edges {
+        node {
+          title
+          description
+          image {
+            childImageSharp {
+              gatsbyImageData(placeholder: BLURRED)
+            }
+          }
         }
       }
     }
